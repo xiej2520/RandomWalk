a5fb0836f2a366e0afede9dc822297081e8efa30
Jacky Xie <jackyxie2520@outlook.com>
2021-03-16 18:48:35 -0400

    Added stats script

---
+++ stats.py
@@ -0,0 +1,34 @@
+from walkers import *
+from imageGen import *
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+lengths = []
+for i in range(0,10000):
+	x = Walker([0,0], [-16, 16, -16, 16])
+	x.basicWalk()
+	lengths.append(len(x.path))
+
+
+xbar = np.mean(lengths)
+s = np.std(lengths)
+min = np.amin(lengths)
+max = np.amax(lengths)
+range = max-min
+print(xbar, s, min, max, range)
+
+
+plt.hist(lengths, np.amax((1, range)))
+
+import os, psutil; print("RAM usage (MB): " + str(psutil.Process(os.getpid()).memory_info().rss / 1024 ** 2))
+plt.show()
+
+
+""" 
+1 Million samples for Walker([0, 0], [-16, 16, -16, 16])
+mean=302.595826
+std=212.4492923014284
+min=21
+max=3048
+"""
